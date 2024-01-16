61196d8e503093d9babb5be1b5e26e05949c5761
Charbrose <124835997+Charbrose@users.noreply.github.com>
2024-01-16 11:45:39 -0500

    hello

--- Lab1.java
+++ Lab1.java
@@ -1,3 +1,12 @@
+/** Project: Lab1
+ * Purpose Details:test
+ * Course: IST242
+ * Author:Charlie Defelice
+ * Date Developed:1/16/24
+ * Last Date Changed:1/16/24
+ * Rev:
+
+ */
 public class Lab1 {
     public static void main(String[] args) {
         System.out.println("Howdy");
