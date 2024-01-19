840d0529e51be9a46be0ea24b240ca967ff320f1
Charbrose <124835997+Charbrose@users.noreply.github.com>
2024-01-19 16:24:37 -0500

    hello

---
+++ Motorcycle.java
@@ -0,0 +1,80 @@
+/**
+ * Project: Motorcycle Project
+ * Purpose Details: Representing Motorcycle objects
+ * Course: IST 242
+ * Author: Charlie Defelice
+ * Date Developed: January 17, 2024
+ * Last Date Changed: January 19, 2024
+ * Rev: 1.3
+ */
+
+public class Motorcycle
+{
+
+    // Fields
+    private String brand;
+    private String model;
+    private int year;
+    private double price;
+
+    // Constructor
+    public Motorcycle(String brand, String model, int year, double price)
+    {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+
+    // Getter methods
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // Setter methods (optional)
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public static void main(String[] args)
+    {
+        // Creating three Motorcycle objects
+        Motorcycle bike1 = new Motorcycle("Honda", "CBR500R", 2022, 7000.00);
+        Motorcycle bike2 = new Motorcycle("Kawasaki", "Ninja 650", 2021, 7500.00);
+        Motorcycle bike3 = new Motorcycle("Yamaha", "YZF-R6", 2023, 10000.00);
+
+        // Using getter methods to print object values
+        System.out.println("Motorcycle 1: " + bike1.getBrand() + " " + bike1.getModel() +
+                " (" + bike1.getYear() + ") - $" + bike1.getPrice());
+
+        System.out.println("Motorcycle 2: " + bike2.getBrand() + " " + bike2.getModel() +
+                " (" + bike2.getYear() + ") - $" + bike2.getPrice());
+
+        System.out.println("Motorcycle 3: " + bike3.getBrand() + " " + bike3.getModel() +
+                " (" + bike3.getYear() + ") - $" + bike3.getPrice());
+    }
+}
