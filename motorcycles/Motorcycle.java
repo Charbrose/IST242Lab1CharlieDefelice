4223e218d88d4158aac68aa5a8f7a8fc6d44d0f6
Charbrose <124835997+Charbrose@users.noreply.github.com>
2024-01-19 17:02:39 -0500

    hello

--- motorcycles/Motorcycle.java
+++ motorcycles/Motorcycle.java
@@ -8,9 +8,9 @@
  * Rev: 1.3
  */
 
+package motorcycles;
 public class Motorcycle
 {
-
     //Fields
     private String brand;
     private String model;
@@ -27,47 +27,61 @@ public class Motorcycle
     }
 
     //Getter methods
-    public String getBrand() {
+    public String getBrand()
+    {
         return brand;
     }
 
-    public String getModel() {
+    public String getModel()
+    {
         return model;
     }
 
-    public int getYear() {
+    public int getYear()
+    {
         return year;
     }
 
-    public double getPrice() {
+    public double getPrice()
+    {
         return price;
     }
 
     //Setter methods
-    public void setBrand(String brand) {
+    public void setBrand(String brand)
+    {
         this.brand = brand;
     }
 
-    public void setModel(String model) {
+    public void setModel(String model)
+    {
         this.model = model;
     }
 
-    public void setYear(int year) {
+    public void setYear(int year)
+    {
         this.year = year;
     }
 
-    public void setPrice(double price) {
+    public void setPrice(double price)
+    {
         this.price = price;
     }
 
     public static void main(String[] args)
     {
-        //Creating three Motorcycle objects
+        //Creating Motorcycle Objects
         Motorcycle bike1 = new Motorcycle("Honda", "CBR500R", 2022, 7000.00);
         Motorcycle bike2 = new Motorcycle("Kawasaki", "Ninja 650", 2021, 7500.00);
         Motorcycle bike3 = new Motorcycle("Yamaha", "YZF-R6", 2023, 10000.00);
 
-        //Using getter methods to print object values
+        //Using setter methods
+        bike1.setBrand("Suzuki");
+        bike2.setModel("GSX-R750");
+        bike3.setYear(2022);
+        bike3.setPrice(11000.00);
+
+        //Using getter methods to print updated object values
         System.out.println("Motorcycle 1: " + bike1.getBrand() + " " + bike1.getModel() +
                 " (" + bike1.getYear() + ") - $" + bike1.getPrice());
 
